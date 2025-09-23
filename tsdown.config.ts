cba62758baeadf44fd87201faad6e71c539b565d
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2025-09-23 20:05:28 +0300

    chore: add tsconfig

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -6,11 +6,13 @@ export default defineConfig([
         format: ["esm", "cjs"],
         dts: true,
         outDir: "build",
+        tsconfig: "./tsconfig.json",
     },
     {
         entry: ["src/index.shared.ts"],
         format: ["esm"],
         dts: true,
         outDir: "build-shared",
+        tsconfig: "./tsconfig.json",
     },
 ]);
